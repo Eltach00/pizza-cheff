137632492476f57be52423c516c00b4f615e9cc3
Eltach00 <eltacquliyev@gmail.com>
2023-05-21 19:35:10 +0400

    main is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { OrderFormComponent } from './order-form/order-form.component';
+import { PizzaOptionComponent } from './pizza-option/pizza-option.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainContentComponent,
-    OrderFormComponent
+    OrderFormComponent,
+    PizzaOptionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pizza-option/pizza-option.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-option',
+  templateUrl: './pizza-option.component.html',
+  styleUrls: ['./pizza-option.component.scss']
+})
+export class PizzaOptionComponent {
+
+}
